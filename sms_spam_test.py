6a2c51c6849a26fbce7f393edf349d5cb9bb272b
Vlad <osetrovlad10@gmail.com>
2025-07-17 20:11:56 +0400

    precision recall and f1 metrics added

--- sms_spam_test.py
+++ sms_spam_test.py
@@ -20,3 +20,14 @@ print(f"Train accuracy: {(train_df["spam"] == train_df["predicted"]).mean()}")
 
 test_df["predicted"] = test_df['email'].apply(nbc.classify)
 print(f"Test accuracy: {(test_df["spam"] == test_df["predicted"]).mean()}")
+
+true_positives = ((test_df["spam"] == test_df["predicted"]) & (test_df["spam"] == 1)).sum()
+false_positives = ((test_df["spam"] != test_df["predicted"]) & (test_df["spam"] == 0)).sum()
+false_negatives = ((test_df["spam"] != test_df["predicted"]) & (test_df["spam"] == 1)).sum()
+precision = true_positives / (true_positives + false_positives)
+recall = true_positives / (true_positives + false_negatives)
+f1 = 2*precision*recall/(precision + recall)
+
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
